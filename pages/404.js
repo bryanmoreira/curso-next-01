bba500d48b9158df08381f0adcd5e66f4d78bcdd
Bryan Moreira <49196485+bryanmoreira@users.noreply.github.com>
2023-10-22 17:25:15 -0300

    Primeiro deploy

---
+++ pages/404.js
@@ -0,0 +1,10 @@
+import Link from '../src/components/Link';
+
+export default function Page404 () {
+    return (
+        <div>
+            <h1>Está perdido?</h1>
+            <Link href="/">Ir para a Home Page</Link>
+        </div>
+    )
+}
